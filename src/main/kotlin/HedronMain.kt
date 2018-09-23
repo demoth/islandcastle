e560e7ba0b5e701ecfe7095ce45e5cb96f74f2dc
demoth <bubnov.d.e@gmail.com>
2018-09-23 21:47:13 +0300

    initial

---
+++ src/main/kotlin/HedronMain.kt
@@ -0,0 +1,50 @@
+package org.demoth.hedron
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.audio.AudioDevice
+import com.badlogic.gdx.audio.Sound
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+fun main(args: Array<String>) {
+    val config = LwjglApplicationConfiguration()
+    LwjglApplication(Hedron(), config)
+}
+
+class Hedron : ApplicationAdapter() {
+    lateinit var batch: SpriteBatch
+    lateinit var img: Texture
+    lateinit var audio: AudioDevice
+    lateinit var audioFile: Sound
+    var played = false
+
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+        audio = Gdx.audio.newAudioDevice(44100, false)
+        audioFile = Gdx.audio.newSound(Gdx.files.internal("camera1.wav"))
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+        if (!played) {
+            audioFile.play()
+            played = true
+        }
+
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
