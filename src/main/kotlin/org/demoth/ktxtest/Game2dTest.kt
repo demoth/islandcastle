14e914ab2167226ee392b7640e0c39273e7f60ff
demoth <bubnov.d.e@gmail.com>
2018-10-01 20:32:40 +0300

    + add solid walls

--- src/main/kotlin/org/demoth/ktxtest/Game2dTest.kt
+++ src/main/kotlin/org/demoth/ktxtest/Game2dTest.kt
@@ -6,9 +6,11 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
@@ -18,9 +20,8 @@ import ktx.box2d.createWorld
 import ktx.graphics.use
 
 
-const val MIN_SPEED = 0.01f
 const val MAX_SPEED = 10f
-const val SPEED_DECEL = 2f
+const val SPEED_DECEL = 5f
 const val SIGHT_RADIUS = 16
 const val TILE_SIZE = 32f
 const val WALK_FORCE = 20f
@@ -49,12 +50,29 @@ class Game2dTest : KtxApplicationAdapter {
         camera = OrthographicCamera(TILE_SIZE, TILE_SIZE)
         viewport = FitViewport(TILE_SIZE * SIGHT_RADIUS, TILE_SIZE * SIGHT_RADIUS, camera)
 
+        map.layers.forEach { layer ->
+            if (layer.name.startsWith("solid_")) {
+                layer.objects.getByType(RectangleMapObject::class.java).forEach { obj ->
+                    val rectangleMapObject = obj as RectangleMapObject
+                    world.body {
+                        type = BodyDef.BodyType.StaticBody
+
+                        val newPosition = Vector2()
+                        rectangleMapObject.rectangle.getCenter(newPosition)
+                        newPosition.scl(1 / PPM)
+                        position.set(newPosition)
+                        box(width = rectangleMapObject.rectangle.width / PPM, height = rectangleMapObject.rectangle.height / PPM)
+                    }
+                }
+            }
+        }
+
         playerBody = world.body {
             // todo use start position
             position.x = 16f * TILE_SIZE / PPM
             position.y = 16f * TILE_SIZE / PPM
             type = BodyDef.BodyType.DynamicBody
-            linearDamping = 5f
+            linearDamping = SPEED_DECEL
             fixedRotation = true
             circle(0.5f)
         }
