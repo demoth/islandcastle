0fa0efe8de5b65905efc12c80357446828cc0ff2
demoth <bubnov.d.e@gmail.com>
2018-10-01 18:09:58 +0300

    + tmx load + wasdf controls

--- src/main/kotlin/org/demoth/ktxtest/HedronMain.kt
+++ src/main/kotlin/org/demoth/ktxtest/HedronMain.kt
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 
 fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration()
-    config.title = "Hello Daniil"
+    config.title = "Topory7 0.0.1"
     config.width = 1000
     config.height = 1000
     config.forceExit = false
