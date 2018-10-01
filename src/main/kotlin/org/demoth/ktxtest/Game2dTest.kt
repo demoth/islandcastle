d7dff5826db184ccff4385fe8325c9f4e392185f
demoth <bubnov.d.e@gmail.com>
2018-10-01 16:39:42 +0300

    + tmx load + wasdf controls

--- src/main/kotlin/org/demoth/ktxtest/Game2dTest.kt
+++ src/main/kotlin/org/demoth/ktxtest/Game2dTest.kt
@@ -6,51 +6,65 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import com.badlogic.gdx.physics.box2d.*
-import ktx.app.KtxApplicationAdapter
-import ktx.box2d.createWorld
-import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
+import ktx.app.KtxApplicationAdapter
 import ktx.box2d.body
+import ktx.box2d.createWorld
 import ktx.graphics.use
 
 
+const val MIN_SPEED = 0.01f
+const val MAX_SPEED = 10f
+const val SPEED_DECEL = 2f
+const val SIGHT_RADIUS = 16
+const val TILE_SIZE = 32f
+const val WALK_FORCE = 20f
+const val PPM = 32f // 1 meter - 32 pixels
+
 class Game2dTest : KtxApplicationAdapter {
     lateinit var world: World
-    lateinit var debugRenderer: Box2DDebugRenderer
     lateinit var batch: SpriteBatch
     lateinit var playerTex: Texture
-    lateinit var grassTex: Texture
     lateinit var viewport: Viewport
-    lateinit var orthographicCamera: OrthographicCamera
+    lateinit var camera: OrthographicCamera
     lateinit var playerBody: Body
+    lateinit var map: TiledMap
+    lateinit var box2dRenderer: Box2DDebugRenderer
+    lateinit var tileRenderer: OrthogonalTiledMapRenderer
 
     override fun create() {
         super.create()
         Box2D.init()
         world = createWorld()
-        debugRenderer = Box2DDebugRenderer()
+        box2dRenderer = Box2DDebugRenderer()
         batch = SpriteBatch()
-        playerTex = Texture(Gdx.files.internal("knight2.png"))
-        grassTex = Texture(Gdx.files.internal("grass1.png"))
-
-        orthographicCamera = OrthographicCamera(32f, 24f)
-        viewport = ScreenViewport(orthographicCamera)
+        playerTex = Texture(Gdx.files.internal("knight1.png"))
+        map = TmxMapLoader().load("simple.tmx")
+        tileRenderer = OrthogonalTiledMapRenderer(map, 1f)
+        camera = OrthographicCamera(TILE_SIZE, TILE_SIZE)
+        viewport = FitViewport(TILE_SIZE * SIGHT_RADIUS, TILE_SIZE * SIGHT_RADIUS, camera)
 
         playerBody = world.body {
+            // todo use start position
+            position.x = 16f * TILE_SIZE / PPM
+            position.y = 16f * TILE_SIZE / PPM
             type = BodyDef.BodyType.DynamicBody
-            circle(64f) {
-                density = 0.0001f
-                restitution = 0.5f
-                friction = 100f
-            }
+            linearDamping = 5f
+            fixedRotation = true
+            circle(0.5f)
         }
-
     }
 
     override fun dispose() {
         world.dispose()
-        debugRenderer.dispose()
+        box2dRenderer.dispose()
+        tileRenderer.dispose()
+        map.dispose()
         batch.dispose()
         playerTex.dispose()
     }
@@ -58,38 +72,39 @@ class Game2dTest : KtxApplicationAdapter {
     override fun render() {
 
         handleInput()
-        world.step(1 / 30f, 6, 2)
+        world.step(1 / 60f, 6, 2)
 
         clearScreen()
-        batch.projectionMatrix = orthographicCamera.combined
+        camera.position.set(playerBody.position.x * PPM, playerBody.position.y * PPM, 0f)
+        camera.update()
+
+        tileRenderer.setView(camera)
+        tileRenderer.render()
+
+        batch.projectionMatrix = camera.combined
         batch.use {
-            val size = 4
-            repeat(size) { y ->
-                repeat(size) { x ->
-                    it.draw(grassTex, (x - size / 2f) * grassTex.width, (y - size / 2f) * grassTex.height)
-                }
-            }
-            it.draw(playerTex, playerBody.position.x - playerTex.width / 2f, playerBody.position.y - playerTex.height / 2)
+            it.draw(playerTex, playerBody.position.x * PPM - playerTex.width / 2f, playerBody.position.y * PPM - playerTex.height / 2)
         }
-        debugRenderer.render(world, orthographicCamera.combined)
-        orthographicCamera.update()
+        box2dRenderer.render(world, camera.combined.scl(PPM))
 
     }
 
     private fun handleInput() {
-        val f = 1000f
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            playerBody.applyForceToCenter(0f, f, true)
+        if (Gdx.input.isKeyPressed(Input.Keys.W) && playerBody.linearVelocity.y < MAX_SPEED) {
+            playerBody.applyForceToCenter(0f, WALK_FORCE, true)
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            playerBody.applyForceToCenter(-f, 0f, true)
+        if (Gdx.input.isKeyPressed(Input.Keys.A) && playerBody.linearVelocity.x > -MAX_SPEED) {
+            playerBody.applyForceToCenter(-WALK_FORCE, 0f, true)
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            playerBody.applyForceToCenter(0f, -f, true)
+        if (Gdx.input.isKeyPressed(Input.Keys.S) && playerBody.linearVelocity.y > -MAX_SPEED) {
+            playerBody.applyForceToCenter(0f, -WALK_FORCE, true)
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            playerBody.applyForceToCenter(f, 0f, true)
+        if (Gdx.input.isKeyPressed(Input.Keys.D) && playerBody.linearVelocity.x < MAX_SPEED) {
+            playerBody.applyForceToCenter(WALK_FORCE, 0f, true)
         }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE))
+            println("pos: ${playerBody.position}, speed: ${playerBody.linearVelocity}")
     }
 
     private fun clearScreen() {
--- src/main/kotlin/org/demoth/ktxtest/HedronMain.kt
+++ src/main/kotlin/org/demoth/ktxtest/HedronMain.kt
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration()
     config.title = "Hello Daniil"
-    config.width = 2000
+    config.width = 1000
     config.height = 1000
     config.forceExit = false
     LwjglApplication(Game2dTest(), config)
