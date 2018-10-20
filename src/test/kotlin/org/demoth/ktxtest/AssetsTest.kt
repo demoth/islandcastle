06d2e8dabd618720ca988808623068c92d0d6822
Julia <julia.vikulina@gmail.com>
2018-10-21 00:42:50 +0300

    extend test to check existence of all assets

--- src/test/kotlin/org/demoth/ktxtest/AssetsTest.kt
+++ src/test/kotlin/org/demoth/ktxtest/AssetsTest.kt
@@ -2,17 +2,26 @@ package org.demoth.ktxtest
 
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import java.io.File
 
 
-class AssetsTest {
-
-    @Test
-    fun `sprites exist`() {
-        Sprites.values().forEach {
-            assertTrue("Sprite [${it.filename}] does not exist!", File("assets/sprites/${it.filename}").exists())
+@RunWith(Parameterized::class)
+class AssetsTest(private val filename: String) {
+    companion object {
+        @Parameterized.Parameters(name = "{0}")
+        @JvmStatic
+        fun data(): Collection<Array<String>> {
+            return Sprites.values().map { arrayOf("assets/sprites/${it.filename}") }.union(
+                    Sounds.values().map { arrayOf("assets/sounds/${it.filename}") }).union(
+                    SpriteSheets.values().map { arrayOf("assets/spriteSheets/${it.filename}") }
+            )
         }
     }
 
-
+    @Test
+    fun `asset exists`() {
+        assertTrue("Asset [$filename] does not exist!", File(filename).exists())
+    }
 }
